fc80ee7f0f96cb572e12604fc68be95de172ffbb
jinhyy <able0527@gmail.com>
2019-03-11 16:59:53 +0900

    4. EventController post요청 테스트 코드 작성

--- src/main/java/me/huk/restapispring/event/EventController.java
+++ src/main/java/me/huk/restapispring/event/EventController.java
@@ -1,9 +1,26 @@
 package me.huk.restapispring.event;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import java.net.URI;
+
+@Controller
+@RequestMapping(value= "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
 
-
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event){
+        URI createdUri = ControllerLinkBuilder.
+                linkTo(EventController.class).slash("{id}").toUri();
+        // created를 하려면 URI가 존재해야하고, Uri는 Hateoas의
+        // ControllerLinkBuilder의 linkTo와 methodOn 기능을 사용하면 만들기 쉽다.
+        event.setId(3);
+        return ResponseEntity.created(createdUri).body(event);
+    }
 }
--- src/test/java/me/huk/restapispring/event/EventControllerTest.java
+++ src/test/java/me/huk/restapispring/event/EventControllerTest.java
@@ -1,5 +1,6 @@
 package me.huk.restapispring.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -18,14 +23,34 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() {
+        Event event = Event.builder()
+                .name("jh-event").description("jh-des")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,3,11,16,44))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,3,11,16,50))
+                .beginEventDateTime(LocalDateTime.of(2019,3,15,18,30))
+                .endEventDateTime(LocalDateTime.of(2019,3,15,20,30))
+                .basePrice(100).maxPrice(200).limitOfEnrollment(100).location("수서역")
+                .build();
+
         try {
-            mockMvc.perform(post("api/events/")
+            mockMvc.perform(post("/api/events/")
+                    // header 설정
                     .contentType(MediaType.APPLICATION_JSON_UTF8)   // 요청응답 json 으로 보내겠다.
                     .accept(MediaTypes.HAL_JSON)    // HAL_JSON 타입 응답만 허용하겠다.
+
+                    //body 설정
+                    .content(objectMapper.writeValueAsString(event))    // object Mapper로 event 요청 json 형식으로 바꾼다.
             )
-                    .andExpect(status().isCreated());
+                    .andDo(print())
+                    .andExpect(status().isCreated())   // created : 201 상태코드
+                    .andExpect(jsonPath("id").exists());
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
